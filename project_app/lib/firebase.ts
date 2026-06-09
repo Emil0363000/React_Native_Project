178f519dae26f8328c2f7153d4b8e22d9772d353
Emil0363000 <mimi03600@gmail.com>
2026-06-09 04:01:19 +0200

    edit

--- project_app/lib/firebase.ts
+++ project_app/lib/firebase.ts
@@ -5,14 +5,7 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBSBlpnKBwKyvLJ0881JhKaZ6EPxXW3PBQ",
-  authDomain: "app-stats-dcf56.firebaseapp.com",
-  projectId: "app-stats-dcf56",
-  storageBucket: "app-stats-dcf56.firebasestorage.app",
-  messagingSenderId: "812311000399",
-  appId: "1:812311000399:web:e560eff24e4827ea09813a"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
